0225506285e48914af51d7d3c622aef7d864dbd9
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-11-12 07:54:46 +0700

    TST

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello Steven Adrian Corne"}
 
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
