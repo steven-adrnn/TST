e36270cf3ab64d5286ad58c19f403374e81d4baa
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-11-20 09:01:24 +0700

    .

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from supabase import create_client, Client
-import os
 import uvicorn
-
-# Inisialisasi Supabase
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+from fastapi import FastAPI
 
 app = FastAPI()
-
-# OAuth2
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-@app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = supabase.auth.sign_in(email=form_data.username, password=form_data.password)
-    if user.error:
-        raise HTTPException(status_code=400, detail="Invalid credentials")
-    return {"access_token": user.user.id, "token_type": "bearer"}
-
-@app.get("/users/me")
-async def read_users_me(token: str = Depends(oauth2_scheme)):
-    user = supabase.auth.get_user(token)
-    if user.error:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    return user
 
 @app.get("/")
 async def root():
