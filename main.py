a70df6712e918c5dd9d968189717c2606f689464
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-11-20 19:20:23 +0700

    .

--- main.py
+++ main.py
@@ -97,4 +97,4 @@ async def get_tools(current_user: dict = Depends(get_current_user)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
